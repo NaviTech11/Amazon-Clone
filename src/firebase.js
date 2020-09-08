c1d263591a09bec311e609b7e70357a40900fcdb
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-08 15:09:53 -0500

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7JpkYcRDqW_htpRytdKivPUaX3ep5X-U",
+    authDomain: "clone-6176b.firebaseapp.com",
+    databaseURL: "https://clone-6176b.firebaseio.com",
+    projectId: "clone-6176b",
+    storageBucket: "clone-6176b.appspot.com",
+    messagingSenderId: "111733167818",
+    appId: "1:111733167818:web:6863f5104b167ee4dfc633"
+  };
