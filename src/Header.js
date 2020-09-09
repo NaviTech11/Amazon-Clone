ba75fbdb142b4aa3fe1a6067f60743e3a9b32619
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-08 21:00:18 -0500

    Add Checkout and Subtotal Components with Styles

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="header">
-
-            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="nav logo"/>
+            <Link to="/">
+                <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="nav logo"/>
+            </Link>
 
             <div className="header_search">
                 <input className="header_searchInput" type="text"></input>
@@ -27,10 +29,13 @@ function Header() {
                     <span className="header_optionLineOne">Your</span>   
                     <span className="header_optionLineTwo">Prime</span>   
                 </div>
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header_optionalLineTwo header_basketCount">0</span>
-                </div>
+
+                <Link to="/checkout">
+                    <div className="header_optionShoppingCart">
+                        <ShoppingCartIcon />
+                        <span className="header_optionalLineTwo header_cartCount">0</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
